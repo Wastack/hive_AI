3dbee12a3effefa93d76e4199da59fb535bb1382
Bunth Tamás <btomi96@gmail.com>
2019-03-24 12:09:16 +0100

    Add random AI test

--- hivegame/test/random_ai_test.py
+++ hivegame/test/random_ai_test.py
@@ -1,12 +1,40 @@
 from unittest import TestCase
+import time
 
-class TestHexBoard(TestCase):
+from hivegame.AI.random_ai import RandomAI
+from hivegame.environment import Environment
+from hivegame.hive import Hive
+from hivegame.view import HiveView
+
+from unittest import TestCase
+
+class TestRandomAi(TestCase):
     """Verify the HexBoard logic"""
 
     def setUp(self):
-        self.board = HexBoard()
+        self.env = Environment()
+        self.view = HiveView(self.env.hive)
 
-    def test_get_surrounding(self):
+    def test_run_againt_itself(self):
+        print("DEBUG: run_againt_itself start")
+        ai = RandomAI()
+        timeout = time.time() + 10 # 10 sec from now
+        while time.time() < timeout:
+            self.env.reset_game()
+            while self.env.check_victory() == Hive.UNFINISHED:
+                response = ai.step(self.env)
+                if not response:
+                    self.env.exec_cmd("pass")
+                    print("DEBUG: AI passed")
+                    continue
+                (piece, coord) = response
+                if piece.kind == 'G':
+                    print(self.view)
+                    print("DEBUG: choosen action is: ({}, {})".format(piece,coord) )
+                    print("DEBUG: from {}".format(piece.position))
+                self.env.action_piece_to(piece, coord)
+        print("DEBUG: a game finished")
+        self.env.logger.close()
 
 
 if __name__ == '__main__':
