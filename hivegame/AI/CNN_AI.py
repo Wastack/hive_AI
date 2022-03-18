9a19dbfba2216465c2d2df4e22fcc58422413eb0
Liam Cottrell <Liam.Cottrell@users.noreply.github.com>
2022-03-18 15:54:40 +1100

    created neural network with keras and helper functions for CNNModel class. Haven't written tests for it yet or tried running, need to implement a train_test function to properly test

--- hivegame/AI/CNN_AI.py
+++ hivegame/AI/CNN_AI.py
@@ -1,22 +1,92 @@
-from engine.environment.aienvironment import AIEnvironment
+from engine.environment.aienvironment import ai_environment
 from AI.random_player import RandomPlayer
 from arena import Arena
 from engine.hive_utils import dotdict
 from project import ROOT_DIR
 
 import numpy as np
+import time
+import os
 
 from keras.models import *
 from keras.layers import *
-from keras.optimizers import *
+from keras.optimizers import adam_v
 
-class CNNModel(nn.Module):
-    def _init_(self, board_size, action_size, device):
-        
-        super(CNNModel, self).__init__()
+args = dotdict({
+    'lr': 0.001,
+    'dropout': 0.3,
+    'epochs': 10,
+    'batch_size': 64,
+    'cuda': False,
+    'num_channels': 16,
+})
 
-        self.device = device
-        self.board_size = board_size
-        self.action_size = action_size
+class CNNModel():
+    def _init_(self, args):
+
+        # game params
+        self.board_x, self.board_y = ai_environment.getBoardSize()
+        self.action_size = ai_environment.getActionSize()
+        self.args = args
+        print("[INFO] Create nnet with: board_x: {}, board_y: {}, action_size: {}, args: {} ".format(self.board_x,
+                                                                                                    self.board_y,
+                                                                                                    self.action_size,
+                                                                                                    args))
+
+        # Neural Net
+        self.input_boards = Input(shape=(self.board_x, self.board_y))    # s: batch_size x board_x x board_y
+
+        x_image = Reshape((self.board_x, self.board_y, 1))(self.input_boards)                # batch_size  x board_x x board_y x 1
+        h_conv1 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='same', use_bias=False)(x_image)))         # batch_size  x board_x x board_y x num_channels
+        h_conv2 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='same', use_bias=False)(h_conv1)))         # batch_size  x board_x x board_y x num_channels
+        h_conv3 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='valid', use_bias=False)(h_conv2)))        # batch_size  x (board_x-2) x (board_y-2) x num_channels
+        h_conv4 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='valid', use_bias=False)(h_conv3)))        # batch_size  x (board_x-4) x (board_y-4) x num_channels
+        h_conv4_flat = Flatten()(h_conv4)
+        s_fc1 = Dropout(args.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(1024, use_bias=False)(h_conv4_flat))))  # batch_size x 1024
+        s_fc2 = Dropout(args.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(512, use_bias=False)(s_fc1))))          # batch_size x 1024
+        self.pi = Dense(self.action_size, activation='softmax', name='pi')(s_fc2)   # batch_size x self.action_size
+        self.v = Dense(1, activation='tanh', name='v')(s_fc2)                    # batch_size x 1
+
+        self.model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
+        self.model.compile(loss=['categorical_crossentropy','mean_squared_error'], optimizer=adam_v2(args.lr))
+
+    def train(self, examples):
+        """
+        examples: list of examples, each example is of form (board, pi, v)
+        """
+        input_boards, target_pis, target_vs = list(zip(*examples))
+        input_boards = np.asarray(input_boards)
+        target_pis = np.asarray(target_pis)
+        target_vs = np.asarray(target_vs)
+        self.model.fit(x = input_boards, y = [target_pis, target_vs], batch_size = self.args.batch_size, epochs = self.args.epochs)
+
+    def predict(self, board):
+        """
+        board: np array with board
+        """
+        # timing
+        start = time.time()
+
+        # preparing input
+        board = np.array(board[np.newaxis, :, :], dtype=np.float64)
+
+        # run
+        pi, v = self.model.predict(board)
+
+        print('PREDICTION TIME TAKEN : {0:03f}'.format(time.time()-start))
+        return pi[0], v[0]
+
+    def save_model(self, folder='checkpoint', filename='model.h5'):
+        file_path = os.path.join(folder, filename)
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+        self.model.save(file_path)
+
+    def load_model(self, folder='checkpoint', filename='model.h5'):
+        file_path = os.path.join(folder, filename)
+        if not os.path.exists(file_path):
+            raise(RuntimeError("No model in path {}".format(file_path)))
+        self.model = load_model(file_path)
+
 
 
